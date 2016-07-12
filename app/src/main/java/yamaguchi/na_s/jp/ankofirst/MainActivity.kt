c6c07ff6e9f82057dad00785f1b87c2f9c7cbcdf
tomotaka yamaguchi <yamaguchi@na-s.jp>
2016-07-12 16:03:57 +0900

    AnkoComponentを実装したクラスを作成

--- app/src/main/java/yamaguchi/na_s/jp/ankofirst/MainActivity.kt
+++ app/src/main/java/yamaguchi/na_s/jp/ankofirst/MainActivity.kt
@@ -2,18 +2,25 @@ package yamaguchi.na_s.jp.ankofirst
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import org.jetbrains.anko.textView
-import org.jetbrains.anko.verticalLayout
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        MainActivityUi().setContentView(this)
+    }
+}
+
+class MainActivityUi() : AnkoComponent<MainActivity> {
+    override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+
         verticalLayout {
             textView {
                 text = "Hello, Anko!"
             }
         }
+
     }
 }
