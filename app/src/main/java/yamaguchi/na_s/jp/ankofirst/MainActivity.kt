77d161a72c2908a30305a6b9dbb954cecb6141c7
tomo <tomotaka@yamaguchi-mac.local>
2016-07-19 13:44:33 +0900

    MainActivityUiをカスタムビューにする

--- app/src/main/java/yamaguchi/na_s/jp/ankofirst/MainActivity.kt
+++ app/src/main/java/yamaguchi/na_s/jp/ankofirst/MainActivity.kt
@@ -1,10 +1,15 @@
 package yamaguchi.na_s.jp.ankofirst
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.AttributeSet
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.RelativeLayout
+import android.widget.TextView
+import butterknife.bindView
 import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,24 +17,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        MainActivityUi().setContentView(this)
+        setContentView(MainActivityUi(this).createView(
+                AnkoContext.Companion.create(this, this@MainActivity)
+        ).rootView)
     }
 }
 
-class MainActivityUi() : AnkoComponent<MainActivity> {
+class MainActivityUi : FrameLayout, AnkoComponent<MainActivity> {
+
+    constructor(context: Context?) : super(context)
+
+    constructor(context: Context?,
+                attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context?,
+                attrs: AttributeSet?,
+                defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    constructor(context: Context?,
+                attrs: AttributeSet?,
+                defStyleAttr: Int,
+                defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
     val xmlLayoutId = View.generateViewId()
-    var xmlLayout : RelativeLayout? = null
+
+    val textViewId = View.generateViewId()
+    val textView : TextView by bindView(textViewId)
 
     override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
 
         relativeLayout {
 
-            xmlLayout = include<RelativeLayout>(R.layout.xml_layout) {
+            include<RelativeLayout>(R.layout.xml_layout) {
                 id = xmlLayoutId
             }
 
             textView {
+                id = textViewId
                 text = "Hello, Anko!"
                 backgroundColor = Color.BLACK
                 textColor = Color.WHITE
@@ -38,6 +62,6 @@ class MainActivityUi() : AnkoComponent<MainActivity> {
                 rightOf(xmlLayoutId)
                 alignParentRight()
             }
-        }
+        }.apply { this@MainActivityUi.addView(this) }
     }
 }
